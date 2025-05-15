5af22f5e1f842b1e6fbf6fe9e1b02fa38fa22d4e
Walexz01 <adegbiteadewaleoluwaseun@gmail.com>
2025-05-16 00:30:28 +0100

    complete service section + animation

--- script.js
+++ script.js
@@ -110,18 +110,191 @@ document.addEventListener("DOMContentLoaded", (event) => {
   Draggable.create(".top_tags", {
     bounds: ".hero_section",
   });
-});
-// hero Image
-gsap.to(".hero_bottom video", {
-  width: "80%",
-  borderRadius: "10px",
-  duration: 2.5,
-  scrollTrigger: {
-    trigger: ".hero_bottom",
-    start: "top 50%",
-    end: "top 30%",
-    invalidateOnRefresh: true,
-    scrub: 2,
-    ease: "expoScale(0.5,7,none)",
-  },
+  // hero Image
+  gsap.to(".hero_bottom video", {
+    width: "80%",
+    borderRadius: "10px",
+    duration: 2.5,
+    scrollTrigger: {
+      trigger: ".hero_bottom",
+      start: "top 50%",
+      end: "top 30%",
+      invalidateOnRefresh: true,
+      scrub: 2,
+      ease: "expoScale(0.5,7,none)",
+    },
+  });
+  // home_about section
+  let aboutSection_tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#about_section",
+      start: "top 60%",
+      ease: "expoScale(0.5,7,none)",
+      delay: 2,
+    },
+  });
+
+  aboutSection_tl.from(
+    "#about_section_head",
+    {
+      width: 0,
+      duration: 2,
+      paddingInline: 0,
+    },
+    "aboutheadTag"
+  );
+
+  // aboustSection header Text splits animation
+
+  let aboutsubhead = SplitText.create("#about_section_subhead", {
+    type: "words, chars, lines",
+  });
+
+  aboutSection_tl.from(
+    aboutsubhead.lines,
+    {
+      rotationX: -20,
+      transformOrigin: "50% 50% -160px",
+      duration: 1.5,
+      autoAlpha: 0,
+      ease: "power3",
+      stagger: 0.25,
+    },
+    "aboutheadTag+=0.2"
+  );
+  let aboustDescription = SplitText.create(".about_section_description", {
+    type: "words, chars",
+  });
+  aboutSection_tl.from(
+    aboustDescription.chars,
+    {
+      x: 150,
+      opacity: 0,
+      duration: 0.2,
+      ease: "power4",
+      stagger: 0.01,
+    },
+    "aboutheadTag+=0.5"
+  );
+  gsap.to(".about_image_card img", {
+    scale: 0.9,
+    // yoyo: true,
+    // repeat: -1,
+    duration: 3,
+    scrollTrigger: {
+      trigger: ".about_image_card",
+      start: "top 40%",
+      ease: "expoScale(0.5,7,none)",
+      delay: 2,
+      scrub: 2,
+      invalidateOnRefresh: true,
+    },
+  });
+  // services section
+  let serviceSection_tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#service_section",
+      start: "top 60%",
+      ease: "expoScale(0.5,7,none)",
+      delay: 1,
+    },
+  });
+
+  serviceSection_tl.from(
+    "#service_section_head",
+    {
+      width: 0,
+      duration: 2,
+      paddingInline: 0,
+    },
+    "serviceheadTag"
+  );
+  // aboustSection header Text splits animation
+
+  let servicesubhead = SplitText.create("#service_section_subhead", {
+    type: "words, chars, lines",
+  });
+
+  serviceSection_tl.from(
+    servicesubhead.lines,
+    {
+      rotationX: -20,
+      transformOrigin: "50% 50% -160px",
+      duration: 1.5,
+      autoAlpha: 0,
+      ease: "power3",
+      stagger: 0.25,
+    },
+    "serviceheadTag+=0.2"
+  );
+  let serviceDescription = SplitText.create("#service_section_description p", {
+    type: "words, chars",
+  });
+  serviceSection_tl.from(
+    serviceDescription.chars,
+    {
+      x: 150,
+      opacity: 0,
+      duration: 0.1,
+      ease: "power4",
+      stagger: 0.01,
+    },
+    "serviceheadTag+=0.5"
+  );
+  // service_section card
+  let serviceCard1Tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#service_card1",
+      start: "top 60%",
+      end: "top 40%",
+      ease: "expoScale(0.5,7,none)",
+      delay: 1,
+      invalidateOnRefresh: true,
+    },
+  });
+  serviceCard1Tl.from(
+    "#service_card1 .service_card_left",
+    {
+      x: -900,
+      duration: 2,
+    },
+    "HomeserviceCard"
+  );
+  serviceCard1Tl.from(
+    "#service_card1 .service_card_right",
+    {
+      x: 900,
+      duration: 2,
+    },
+    "HomeserviceCard"
+  );
+
+  let serviceCard2Tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#service_card2",
+      start: "top 60%",
+      end: "top 40%",
+      ease: "expoScale(0.5,7,none)",
+      delay: 1,
+      invalidateOnRefresh: true,
+    },
+  });
+  serviceCard2Tl.from(
+    "#service_card2 .service_card_left",
+    {
+      x: -900,
+      duration: 2,
+    },
+    "HomeserviceCard2"
+  );
+  serviceCard2Tl.from(
+    "#service_card2 .service_card_right",
+    {
+      x: 900,
+      duration: 2,
+    },
+    "HomeserviceCard2"
+  );
+
+  //end
 });
